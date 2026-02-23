5f26d01a2e8d24b9e245fbdbcc6c8306d2fdc455
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2026-02-23 17:16:04 +0200

    Install script: warn if ~/.local/bin is not in PATH

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ mkdir -p "$BIN_DIR"
 ln -sf "$HAL_DIR/run" "$BIN_DIR/hal"
 
 echo "Linked $BIN_DIR/hal -> $HAL_DIR/run"
+
+if [[ ":$PATH:" != *":$BIN_DIR:"* ]]; then
+	echo "NOTE: $BIN_DIR is not in your PATH. Add it to your shell profile, e.g.:"
+	echo "  export PATH=\"\$HOME/.local/bin:\$PATH\""
+fi
